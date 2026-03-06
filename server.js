986eb8250f86fd6464e0afd10ac9ec6de5a46ebb
amiconnect2026 <jeanmsantos10@aqui.com>
2026-03-06 12:47:49 +0000

    feat: rotas CRUD bairros de entrega

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const alertasRoutes = require('./routes/alertas');
 const pedidosRoutes = require('./routes/pedidos');
 const empresasRoutes = require('./routes/empresas');
 const socketRoutes = require('./routes/socket');
+const bairrosRoutes = require('./routes/bairros');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/produtos', produtosRoutes);
@@ -48,6 +49,7 @@ app.use('/api/alertas', alertasRoutes);
 app.use('/api/pedidos', pedidosRoutes);
 app.use('/api/empresas', empresasRoutes);
 app.use('/api/socket', socketRoutes);
+app.use('/api/bairros', bairrosRoutes);
 
 // Socket.io - conexões
 io.on('connection', (socket) => {
